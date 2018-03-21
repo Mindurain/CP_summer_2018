909c95fa76fd6958962ee09822d5bffcf9732da6
Mindurain <mindurainlo@alumni.unav.es>
2018-03-21 12:55:47 +0100

    Strings

--- src/pl/waw/sgh/Strings.java
+++ src/pl/waw/sgh/Strings.java
@@ -6,7 +6,7 @@ public class Strings {
     public static void main(String[]args){
 
 
-        char c='a';
+       /* char c='a';
         char d = 68;
 
         char e = 33636;
@@ -14,6 +14,69 @@ public class Strings {
         System.out.println(c);
         System.out.println(d);
         System.out.println(e);
+
+
+String s1 = "abc";
+        System.out.println(s1);
+
+
+        String s2 = s1;
+        System.out.println(s2);
+        s2 = s2 + "d";
+
+        System.out.println(s2);
+
+        String s3 = s1.concat(s2); //s1+s2
+        System.out.println(s3);
+        String s4 = s1.replace("b", "B");
+        System.out.println(s4);
+        char c1 = s1.charAt(2);
+        System.out.println(c1);
+        s1.indexOf("b");
+
+
+        int i = "Hello world".indexOf("l");
+        System.out.println(i);
+
+        int j = "hello World".lastIndexOf("l");
+        System.out.println(j);
+
+
+        boolean sw = "Hello World".startsWith("Hell");
+        System.out.println("s1 len" + s1.length());
+
+*/
+//Comparing strings
+
+        String s1 = "abc";
+        System.out.println(s1);
+        //s2 = s1;
+
+        String s2 = "abc";
+       // s2 = new String ("abc");
+
+        System.out.println(s2);
+//Don't use this to compare objects!!
+        if (s2 == s1) {
+            System.out.println("s2=s1");
+
+        }
+
+        if (s2.equals(s1)) {
+            System.out.println("s2 equals s1");
+        }
+
+
+
+        String s6 = "Hello World";
+        String s7 = s6.substring(7);
+
+        System.out.println(s7);
+
+        String s8 = s6.substring(0,5);
+        System.out.println(s8);
+
+        //s1.equals()
 
     }
 
