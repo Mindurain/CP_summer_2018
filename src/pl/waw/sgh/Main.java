b7118446ca675f3889ba0ff10969d44c22fa2ead
Mindurain <mindurainlo@alumni.unav.es>
2018-03-21 12:15:20 +0100

    2 class: Objects and primitive types

--- src/pl/waw/sgh/Main.java
+++ src/pl/waw/sgh/Main.java
@@ -1,8 +1,12 @@
+
 package pl.waw.sgh;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello!!");
+
+        int a = 5;
+        System.out.println("a=" + a);
     }
 }
---
+++ src/pl/waw/sgh/Strings.java
@@ -0,0 +1,22 @@
+package pl.waw.sgh;
+
+public class Strings {
+
+
+    public static void main(String[]args){
+
+
+        char c='a';
+        char d = 68;
+
+        char e = 33636;
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+    }
+
+
+
+}
